66379711e80bc30d0aea35ef78736586318f7061
PaulShervin <paulshervin77@gmail.com>
2025-05-22 10:36:00 +0530

    done

--- gallery/myapp/apps.py
+++ gallery/myapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class IgalleryappConfig(AppConfig):
+class MyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'igalleryapp'
+    name = 'myapp'
